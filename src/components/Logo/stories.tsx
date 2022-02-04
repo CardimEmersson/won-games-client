3d3a6137aac7a43c827a4e242e21f98c6dbae6d3
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2022-02-03 21:22:33 -0300

    create logo component with storybook and tests

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,10 @@
+// import {Story, Meta} from '@storybook/react/types-'
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { Logo } from '.';
+
+export default {
+  title: 'Logo',
+  component: Logo
+} as Meta;
+
+export const Basic: Story = (args) => <Logo {...args} />;
