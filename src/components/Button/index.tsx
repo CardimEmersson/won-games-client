dfa0ef1365ee269745ddebc46e91a1c462364fb3
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2022-02-05 18:38:00 -0300

    create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import { ReactNode, MouseEvent } from 'react';
+import * as S from './styles';
+
+export type ButtonProps = {
+  children?: ReactNode;
+  size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean;
+  icon?: ReactNode;
+  onClick?: () => (event: MouseEvent<HTMLButtonElement>) => void;
+};
+
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false,
+  icon,
+  ...props
+}: ButtonProps) => {
+  return (
+    <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+      {!!icon && icon}
+      {!!children && <span>{children}</span>}
+    </S.Wrapper>
+  );
+};
+
+export { Button };
