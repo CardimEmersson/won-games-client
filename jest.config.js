968bea702d7f2994f9bb251403472a3448289894
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2022-02-17 20:54:01 -0300

    create Slider and BannerSlider components

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src', '<rootDir>/.jest']
 };
---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { BannerSlider, BannerSliderProps } from '.';
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/random/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://source.unsplash.com/random/1042x580',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  }
+];
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta;
+
+export const Basic: Story<BannerSliderProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <BannerSlider {...args} />
+  </div>
+);
